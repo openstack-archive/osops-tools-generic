07bcb7e38ef9bacecccae1f0a1577d8a2d73096d
Christian Berendt <berendt@b1-systems.de>
2015-09-29 18:21:47 +0200

    Cleanup errors in shell scripts identified by bashate
    Change-Id: Ib3c4d841e4282368343b81e5824a0319e9817b48

--- libvirt/remove-deleted-orphans.sh
+++ libvirt/remove-deleted-orphans.sh
@@ -7,39 +7,33 @@
 #
 
 removeorphan(){
-	local domain
-	local tmp
-	domain=$1
+    local domain
+    local tmp
+    domain=$1
  
-	tmp=$( virsh destroy $domain )
-	tmp=$( virsh undefine $domain )
-	tmp=$(virsh list --all | grep $domain )
-	if [ $? -eq 1 ]
-	then
-		tmp=$( ps auxwwwf | grep $domain | grep -v grep )
-		if [ $? -eq 1 ]
-		then
-			return 0
-		fi
-	fi
-	return 1
- 
+    tmp=$( virsh destroy $domain )
+    tmp=$( virsh undefine $domain )
+    tmp=$(virsh list --all | grep $domain )
+    if [ $? -eq 1 ]; then
+        tmp=$( ps auxwwwf | grep $domain | grep -v grep )
+        if [ $? -eq 1 ]; then
+            return 0
+        fi
+    fi
+    return 1
 }
  
-for i in /etc/libvirt/qemu/*.xml
-do
-	disklocation=$( grep /var/lib/nova/instances $i | grep disk | cut -d"'" -f2,2)
-	if [ ! -e $disklocation ]
-	then
-		orphan=$(echo $i | cut -d"/" -f5,5 | cut -d"." -f1,1)
-		echo "$orphan does not have a disk located at: $disklocation"
-		echo "This is an orphan of openstack... stopping the orphaned vm."
-		removeorphan $orphan
-		if [ $? -eq 0 ]
-		then
-			echo "Domain $orphan has been shutdown and removed"
-		else
-			echo "Domain $orphan has *NOT* been shutdown and removed"
-		fi
-	fi
+for i in /etc/libvirt/qemu/*.xml; do
+    disklocation=$( grep /var/lib/nova/instances $i | grep disk | cut -d"'" -f2,2)
+    if [ ! -e $disklocation ]; then
+        orphan=$(echo $i | cut -d"/" -f5,5 | cut -d"." -f1,1)
+        echo "$orphan does not have a disk located at: $disklocation"
+        echo "This is an orphan of openstack... stopping the orphaned vm."
+        removeorphan $orphan
+        if [ $? -eq 0 ]; then
+            echo "Domain $orphan has been shutdown and removed"
+        else
+            echo "Domain $orphan has *NOT* been shutdown and removed"
+        fi
+    fi
 done
