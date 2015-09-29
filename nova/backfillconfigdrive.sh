34df06a924f5a8e1d30868b0f09977fcb0c1bb83
Christian Berendt <berendt@b1-systems.de>
2015-09-29 18:21:47 +0200

    Cleanup errors in shell scripts identified by bashate
    Change-Id: Ib3c4d841e4282368343b81e5824a0319e9817b48

--- nova/backfillconfigdrive.sh
+++ nova/backfillconfigdrive.sh
@@ -7,7 +7,6 @@
 # at https://bugs.launchpad.net/nova/+bug/1356534
 #
 # Author: Mike Dorman <mdorman@godaddy.com>
-#
 
 cd /root
 mkdir -p blank
@@ -15,11 +14,10 @@ mkisofs -o blank.iso blank/ >/dev/null 2>&1
 rmdir blank
 
 for i in `ls /var/lib/nova/instances | grep -E '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'`; do
-  ls -l /var/lib/nova/instances/$i/disk.config
-  if [ ! -s /var/lib/nova/instances/$i/disk.config ]; then
-    echo "$i config drive doesn't exist, or is size zero."
-    cp -f /root/blank.iso /var/lib/nova/instances/$i/disk.config
-    chown qemu:qemu /var/lib/nova/instances/$i/disk.config
-  fi
+    ls -l /var/lib/nova/instances/$i/disk.config
+    if [ ! -s /var/lib/nova/instances/$i/disk.config ]; then
+        echo "$i config drive doesn't exist, or is size zero."
+        cp -f /root/blank.iso /var/lib/nova/instances/$i/disk.config
+        chown qemu:qemu /var/lib/nova/instances/$i/disk.config
+    fi
 done
-
