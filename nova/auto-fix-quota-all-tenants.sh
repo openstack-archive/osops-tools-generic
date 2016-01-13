db6cba1de98911faa28af9203ac87a077272b028
Peter Jenkins <maileter-jenkins.comOSGhostInstanceCheck>
2016-01-13 14:06:21 +0200

    Update aut-fix-quota tool for Juno
    I've ported this tool to work with Juno and thought I'd share this for
    all to use.
    
    We've done basic functional testing, an internal code review and used it
    against our production cloud. I would encourage further review and
    testing, but this should be usable for others.
    
    The backend changes to Juno required more changes that I would have
    liked and the code does throw the odd exception (handled by the code)
    when run against every project in our production system, but this hasn't
    caused any issues for us.
    
    Change-Id: I3913251ede4949149e63d1337bd92dd836f98763

--- nova/auto-fix-quota-all-tenants.sh
+++ nova/auto-fix-quota-all-tenants.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-source /root/scripts/stackrc
 
 echo "$(date): Tenant quota correction - started"
 
-for x in $(keystone --insecure tenant-list | awk -F' | ' '!/^\+/ && !/\ id\ / {print $2}'); do
-    echo "Correcting quota for tenant $x"
-    python /root/scripts/auto-fix-quota.py --tenant $x
+for x in $(openstack project list -f csv -c ID --quote none | tail -n +2);
+do
+   #echo "Correcting quota for tenant $x"
+   ./auto-fix-quota.py --quiet --dryrun --tenant $x 
+   #echo
 done
 
 echo "$(date): Tenant quota correction - finished"
