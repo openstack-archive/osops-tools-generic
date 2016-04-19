0b849f430a7edca184682f351782cc7afbe1bc7b
Marcus Furlong <furlongm@gmail.com>
2016-04-19 10:12:15 +1000

    handle nova servers with spaces in the name
    Change-Id: I8cb06860b984dd4bc4d17920bfbe9c78e15ff9ac

--- nova/list-vms-by-host.sh
+++ nova/list-vms-by-host.sh
@@ -5,8 +5,11 @@
 #
 # Author: Mike Dorman <mdorman@godaddy.com>
 
+IFS="
+"
+
 for i in `nova list --all-tenants | grep -v '^+-' | grep -v '^| ID' |\
-            awk '{print $2 "," $4 "," $6;}'`; do
+        cut -d "|" -f 2,3,5 | sed -e "s/ *| */,/g" -e "s/^ *//g"` ; do
     ID=`echo $i | cut -d, -f 1`
     NAME=`echo $i | cut -d, -f 2`
     STATUS=`echo $i | cut -d, -f 3`
